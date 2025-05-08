5d9cddc52965b04957af281be4ccbae000fc1502
Muhammad-Owais-Warsi <warsimuhammadowais@gmail.com>
2025-05-08 21:08:10 +0530

    resolve result page issue

--- frontend2/src/components/result.jsx
+++ frontend2/src/components/result.jsx
@@ -1,12 +1,25 @@
-import { useParams } from "react-router-dom"
+import { useLocation } from "react-router-dom";
 
 export default function Result() {
+  const { search } = useLocation();
+
+  const queryParams = new URLSearchParams(search);
+  const correct = queryParams.get("correct");
+  const error = queryParams.get("error");
+
   
-  const { correct, error } = useParams();  
   return (
     <>
-      <h1>Correct: {correct}</h1>
-      <h1>Error: {error}</h1>
+      {
+        correct && error ? (
+          <div>
+            <h1>Correct: {correct}</h1>
+            <h1>Error: {error}</h1>
+          </div>
+        ) : (
+          <h1>Loading...</h1>
+        )
+      }
     </>
-  )
+  );
 }
